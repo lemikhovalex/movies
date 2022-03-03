56d75782baad5561366f6107405b16425b33a556
Aleksandr Lemikhov <lemikhovalex@yandex.ru>
2022-03-03 16:31:40 +0300

    check all extractors

--- 01_etl/main_etl.py
+++ 01_etl/main_etl.py
@@ -1,8 +1,11 @@
+import json
+
 import psycopg2
 from psycopg2.extras import DictCursor
 
 from app.sqlite_to_postgres.loaders.settings import get_dsl
-from etl.extracters import FilmworkExtracter
+from etl.extracters import FilmworkExtracter, GenreExtracter, PersonExtracter
+from etl.loaders import Loader
 from etl.transformers import PgToESTransformer
 
 
@@ -11,10 +14,15 @@ def main():
     with psycopg2.connect(
         **get_dsl(".env"), cursor_factory=DictCursor
     ) as pg_conn:
-        extracter = FilmworkExtracter(pg_connection=pg_conn, batch_size=1)
-        extracted = extracter.extract()
-    transformed = transformer.transform(extracted)
-    print([es_data.json() for es_data in transformed])
+        for _i, ex_constr in enumerate(
+            (FilmworkExtracter, GenreExtracter, PersonExtracter)
+        ):
+            extracter = ex_constr(pg_connection=pg_conn)
+            for extracted in extracter.extract():
+                transformed = transformer.transform(extracted)
+                loader = Loader(index="movies")
+                loader.load(transformed)
+            print(_i)
 
 
 if __name__ == "__main__":
