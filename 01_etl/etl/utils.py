8bfa6cddb40c6c9f66e679568924edba94784e09
Aleksandr Lemikhov <lemikhovalex@yandex.ru>
2022-03-05 10:58:33 +0300

    opportunity not to raise error (for backoff)

--- 01_etl/etl/utils.py
+++ 01_etl/etl/utils.py
@@ -1,3 +1,4 @@
-def process_exception(exception, logger):
+def process_exception(exception, logger, do_raise: bool = True):
     logger.exception(str(exception))
-    raise exception
+    if do_raise:
+        raise exception
