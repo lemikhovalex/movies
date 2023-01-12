2b8a1425451c1c99c164ab947f0b637b7aadaa44
Aleksandr Lemikhov <lemikhovalex@gmail.com>
2023-01-12 14:13:13 +0530

    clean pg even before start

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,25 @@ import pytest
 from elasticsearch import Elasticsearch
 from psycopg2.extras import DictCursor
 
+from etl.pg_to_es.base import IExtracter
+from etl.pg_to_es.extracters import FilmworkExtracter, GenreExtracter, PersonExtracter
+from etl.state import BaseUniqueStorage, GenericFileStorage, GenericQueue, State
 from tests import constants
 from tests.config import CONFIG
+
+
+def clean_pg(pg_conn):
+    with pg_conn.cursor() as cursor:
+        cursor.execute("TRUNCATE content.person_film_work CASCADE;")
+        pg_conn.commit()
+        cursor.execute("TRUNCATE content.genre_film_work CASCADE;")
+        pg_conn.commit()
+        cursor.execute("TRUNCATE content.genre CASCADE;")
+        pg_conn.commit()
+        cursor.execute("TRUNCATE content.person CASCADE;")
+        pg_conn.commit()
+        cursor.execute("TRUNCATE content.film_work CASCADE;")
+        pg_conn.commit()
 
 
 @pytest.fixture(scope="session")
@@ -27,19 +44,9 @@ def pg_conn():
         "port": CONFIG.db_port,
     }
     with psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
+        clean_pg(pg_conn)
         yield pg_conn
-
-        with pg_conn.cursor() as cursor:
-            cursor.execute("TRUNCATE content.person_film_work CASCADE;")
-            pg_conn.commit()
-            cursor.execute("TRUNCATE content.genre_film_work CASCADE;")
-            pg_conn.commit()
-            cursor.execute("TRUNCATE content.genre CASCADE;")
-            pg_conn.commit()
-            cursor.execute("TRUNCATE content.person CASCADE;")
-            pg_conn.commit()
-            cursor.execute("TRUNCATE content.film_work CASCADE;")
-            pg_conn.commit()
+        clean_pg(pg_conn)
 
 
 @pytest.fixture(scope="session")
@@ -71,3 +78,35 @@ def es_factory() -> Generator[Callable[[], Elasticsearch], None, None]:
 @pytest.fixture(scope="session")
 def es_conn(es_factory: Callable[[], Elasticsearch]):
     return es_factory()
+
+
+@pytest.fixture(scope="session")
+def fw_queue() -> BaseUniqueStorage:
+    return GenericQueue()
+
+
+@pytest.fixture(scope="session")
+def person_extracter(pg_conn) -> IExtracter:
+    return PersonExtracter(
+        pg_connection=pg_conn,
+        state=State(GenericFileStorage()),
+        batch_size=50,
+    )
+
+
+@pytest.fixture(scope="session")
+def genre_extracter(pg_conn) -> IExtracter:
+    return GenreExtracter(
+        pg_connection=pg_conn,
+        state=State(GenericFileStorage()),
+        batch_size=2,
+    )
+
+
+@pytest.fixture(scope="session")
+def fw_extracter(pg_conn) -> IExtracter:
+    return FilmworkExtracter(
+        pg_connection=pg_conn,
+        state=State(GenericFileStorage()),
+        batch_size=10,
+    )
