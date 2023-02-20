32b515c14788788b56586755cbe69d3fa0f0c83e
Aleksandr <lemikhovalex@gmail.com>
2023-02-20 12:00:34 +0530

    try read postgres

--- tests/test_spark.py
+++ tests/test_spark.py
@@ -11,21 +11,23 @@ def get_spark_builder(spark_host: str, spark_port: int, app_name: str):
 
 spark_session = get_spark_builder(spark_host="spark", spark_port=7077, app_name="test")
 spark_session = spark_session.config(
-    "spark.jars", "{}/drivers/sqlite-jdbc-3.34.0.jar".format(SPARK_DIR)
+    "spark.jars", "{}/drivers/postgresql-42.2.6.jar".format(SPARK_DIR)
 ).config("spark.driver.extraClassPath", "{}/sqlite-jdbc-3.34.0.jar".format(SPARK_DIR))
 print(1)
 spark_session = spark_session.getOrCreate()
 print(2)
-driver = "org.sqlite.JDBC"
+driver = "org.postgresql.Driver"
 path = "etl/db.sqlite"
-url = "jdbc:sqlite:" + path
-tablename = "film_work"
+url = "jdbc:postgresql://admin_db:15432/movies"
+tablename = "content.genre"
 print(3)
 dbDataFrame = (
     spark_session.read.format("jdbc")
     .option("url", url)
     .option("dbtable", tablename)
     .option("driver", driver)
+    .option("user", "app")
+    .option("password", "app")
     .load()
 )
 print(4)
