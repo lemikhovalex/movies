3362ad598ec6d5aa91451fc0346f5b69ded0f00c
Aleksandr <lemikhovalex@gmail.com>
2023-02-20 13:32:10 +0530

    dull test for connection

--- tests/test_spark.py
+++ tests/test_spark.py
@@ -1,35 +1,27 @@
 from pyspark.sql import SparkSession
 
-SPARK_DIR = "/app_data/spark"
+SPARK_DIR = "/opt/workspace"
 
+spark_session = (
+    SparkSession.builder.appName("Python Spark SQL basic example")
+    .config("spark.driver.memory", "4g")
+    .config("spark.executor.memory", "2g")
+    .config("spark.driver.maxResultSize", "1g")
+    .config("spark.driver.extraClassPath", f"{SPARK_DIR}/postgresql-42.5.1.jar")
+    .getOrCreate()
+)
+print(f"{SPARK_DIR}/postgresql-42.5.1.jar")
 
-def get_spark_builder(spark_host: str, spark_port: int, app_name: str):
-    return SparkSession.builder.appName(app_name).master(
-        f"spark://{spark_host}:{spark_port}"
-    )
-
-
-spark_session = get_spark_builder(spark_host="spark", spark_port=7077, app_name="test")
-spark_session = spark_session.config(
-    "spark.jars", "{}/drivers/postgresql-42.2.6.jar".format(SPARK_DIR)
-).config("spark.driver.extraClassPath", "{}/sqlite-jdbc-3.34.0.jar".format(SPARK_DIR))
-print(1)
-spark_session = spark_session.getOrCreate()
-print(2)
-driver = "org.postgresql.Driver"
-path = "etl/db.sqlite"
-url = "jdbc:postgresql://admin_db:15432/movies"
-tablename = "content.genre"
-print(3)
 dbDataFrame = (
     spark_session.read.format("jdbc")
-    .option("url", url)
-    .option("dbtable", tablename)
-    .option("driver", driver)
+    .option("url", "jdbc:postgresql://admin_db:15432/movies")
+    .option("dbtable", "content.genre")
     .option("user", "app")
     .option("password", "app")
-    .load()
+    .option("driver", "org.postgresql.Driver")
 )
-print(4)
-dbDataFrame.show()
-print(5)
+print(2)
+dbDataFrame = dbDataFrame.load()
+print(3)
+dbDataFrame.show(5)
+print(3)
