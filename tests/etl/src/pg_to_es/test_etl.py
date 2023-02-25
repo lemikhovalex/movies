53f1f5bb099f636b532f4fdb584ef747baa8b24c
Aleksandr Lemikhov <lemikhovalex@gmail.com>
2023-02-25 10:54:47 +0000

    add spark etl to root stupidly

--- tests/etl/src/pg_to_es/test_etl.py
+++ tests/etl/src/pg_to_es/test_etl.py
@@ -2,11 +2,15 @@ import datetime
 import time
 import urllib.parse
 from abc import ABC
+from collections import OrderedDict
 from http import HTTPStatus
 
 import pytest
 import requests
 from elasticsearch import Elasticsearch
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json
+from pyspark.sql.types import ArrayType, MapType, StringType
 from requests.auth import HTTPBasicAuth
 
 from etl.pg_to_es.extracters import IPEMExtracter, TargetExtracer
@@ -233,9 +237,161 @@ class FillESAF(ABC):
         assert is_success, status
 
 
+class FillESSpark(ABC):
+    def test_spark_etl(self, request):
+        request.getfixturevalue("pg_conn")
+        request.getfixturevalue("es_factory")
+        spark_jars = [
+            # "org.apache.hadoop:hadoop-aws:3.3.1",
+            "org.postgresql:postgresql:42.2.10",
+            "org.elasticsearch:elasticsearch-spark-30_2.12:8.6.2",
+        ]
+        spark_session = (
+            SparkSession.builder.master("spark://spark-master:7077")
+            .appName("Python Spark SQL basic example")
+            .config("spark.driver.memory", "4g")
+            .config("spark.executor.memory", "2g")
+            .config("spark.driver.maxResultSize", "1g")
+            .config("spark.jars.packages", ",".join(spark_jars))
+            .getOrCreate()
+        )
+        query = """
+            select
+                fw.id as id,
+                fw.rating  as imdb_rating,
+                json_agg(DISTINCT g.name) as genres_names,
+                fw.title as title,
+                fw.description as description,
+                coalesce(
+                    json_agg(DISTINCT p.full_name)
+                    FILTER (WHERE p_fw.role = 'actor' and p.id is not NULL),
+                    '[]'
+                ) actors_names,
+                coalesce(
+                    json_agg(DISTINCT p.full_name)
+                    FILTER (WHERE p_fw.role = 'director' and p.id is not NULL),
+                    '[]'
+                ) directors_names,
+                coalesce(
+                    json_agg(DISTINCT p.full_name)
+                    FILTER (WHERE p_fw.role = 'writer' and p.id is not NULL),
+                    '[]'
+                ) writers_names,
+                coalesce (
+                json_agg(
+                    DISTINCT jsonb_build_object(
+                        'id', p.id,
+                        'name', p.full_name
+                    )
+                ) FILTER (WHERE p_fw.role = 'actor' and p.id is not NULL),
+                '[]'
+            ) as actors,
+                coalesce (
+                json_agg(
+                    DISTINCT jsonb_build_object(
+                        'id', p.id,
+                        'name', p.full_name
+                    )
+                ) FILTER (WHERE p_fw.role = 'writer' and p.id is not NULL),
+                '[]'
+            ) as writers,
+                coalesce (
+                json_agg(
+                    DISTINCT jsonb_build_object(
+                        'id', p.id,
+                        'name', p.full_name
+                    )
+                ) FILTER (WHERE p_fw.role = 'director' and p.id is not NULL),
+                '[]'
+            ) as directors,
+                coalesce (
+                json_agg(
+                    DISTINCT jsonb_build_object(
+                        'id', g.id,
+                        'name', g.name
+                    )
+                ) FILTER (WHERE p_fw.role = 'director' and p.id is not NULL),
+                '[]'
+            ) as genres
+            from content.film_work as fw
+            join content.genre_film_work as g_fw on g_fw.film_work_id = fw.id
+            join content.genre as g on g.id = g_fw.genre_id
+            join content.person_film_work as p_fw on p_fw.film_work_id = fw.id
+            join content.person as p on p_fw.person_id = p.id
+            group by fw.id
+        """
+
+        dbDataFrame = (
+            spark_session.read.format("jdbc")
+            .option("url", "jdbc:postgresql://admin_db:15432/movies")
+            .option("driver", "org.postgresql.Driver")
+            .option("user", "app")
+            .option("password", "app")
+            .option(
+                "query",
+                query,
+            )
+        )
+
+        print(2)
+        dbDataFrame = dbDataFrame.load()
+        print(3)
+        for c in ["actors", "writers", "genres", "directors"]:
+            dbDataFrame = dbDataFrame.withColumn(
+                c,
+                from_json(
+                    getattr(dbDataFrame, c),
+                    MapType(
+                        StringType(),
+                        StringType(),
+                    ),
+                ),
+            )
+        for c in ["genres_names", "directors_names", "actors_names", "writers_names"]:
+            dbDataFrame = dbDataFrame.withColumn(
+                c,
+                from_json(
+                    getattr(dbDataFrame, c),
+                    ArrayType(StringType()),
+                ),
+            )
+        dbDataFrame.show(5)
+        print(4)
+
+        # Write the result into ES
+        options = OrderedDict()
+        options["es.nodes"] = "elasticsearch"
+        options["es.port"] = "9200"
+        options["es.Resource"] = "movies"
+        # Connect the timeout time setting of the ES. Default 1M
+        options["es.http.timeout"] = "10000m"
+        options["es.nodes.wan.only"] = "true"
+        # Default retry 3 times, if the negative value is an unlimited retry (careful)
+        options["es.batch.write.retry.count"] = "15"
+        # Default Retry Waiting Time is 10s
+        options["es.batch.write.retry.wait"] = "60"
+        # The following parameters can control the amount and number
+        # of data quantities and
+        # numbers written in a single batch (two choices)
+        options["es.batch.size.bytes"] = "2mb"
+        options["es.batch.size.entries"] = "128"
+
+        # # elasticsearch-spark-20_2.10-7.17.9.jar
+        _ = (
+            dbDataFrame.write.format("org.elasticsearch.spark.sql")
+            .options(**options)
+            .mode("overwrite")
+            .save()
+        )
+
+
 class TestPlainPgToES(BaseTests, FillESPlain):
     ...
 
 
 class TestAFPgToES(BaseTests, FillESAF):
+    ...
+
+
+class TestSparkETL(BaseTests, FillESSpark):
     ...
