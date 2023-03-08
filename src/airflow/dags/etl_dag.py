6cd28bdf81b59da90fe776ff11f4b653184f0272
Aleksandr <lemikhovalex@gmail.com>
2023-03-08 22:04:48 +0530

    add clean redis command, use x-com

--- src/airflow/dags/etl_dag.py
+++ src/airflow/dags/etl_dag.py
@@ -1,7 +1,22 @@
+import uuid
+
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 
-from airflow import models
+from airflow import XComArg, models
+
+
+def get_q_name_with_id(q: str, id_: str) -> str:
+    out = f"q-{id_}"
+    out = out.replace("-", "_")
+    return out
+
+
+def get_uuid(ti):
+    ti.xcom_push(key="id", value=str(uuid.uuid4()))
+    return
+
 
 with models.DAG(
     dag_id="movies_etl_pg_to_es",
@@ -10,12 +25,20 @@ with models.DAG(
     tags=["etl"],
 ) as dag:
 
+    init_node = PythonOperator(task_id="init_uuid_gen", python_callable=get_uuid)
+
+    final_node = BashOperator(
+        task_id="final-operator",
+        bash_command="echo 0",
+        dag=dag,
+    )
+
     fill_fw_es = BashOperator(
         task_id="fill_es_fw_from_q",
         bash_command=" ".join(
             [
                 "cd /srv/app/src/etl/ && python cli.py fill-es-from-q",
-                "--queue=fw --batch-size=256",
+                f"--queue=fw-{XComArg(init_node, key='id')} --batch-size=256",
             ]
         ),
         dag=dag,
@@ -32,7 +55,22 @@ with models.DAG(
                 [
                     "cd /srv/app/src/etl/ && python cli.py fill-base-q",
                     f"--extracter={extracter}  --batch-size={batch_size}",
-                    f"--queue-name={queue_name}",
+                    f"--queue-name={queue_name}-{XComArg(init_node, key='id')}",
+                ]
+            ),
+            dag=dag,
+        )
+        # clean_q = BashOperator(
+        #     task_id=f"clean_{queue_name}_queue",
+        #     bash_command="echo 0",
+        #     dag=dag,
+        # )
+        clean_q = BashOperator(
+            task_id=f"clean_{queue_name}_queue",
+            bash_command=" ".join(
+                [
+                    "cd /srv/app/src/etl/ && python cli.py clean-q",
+                    f"--queue={queue_name}-{XComArg(init_node, key='id')}",
                 ]
             ),
             dag=dag,
@@ -45,15 +83,18 @@ with models.DAG(
                     [
                         "cd /srv/app/src/etl/ && python cli.py fill-q-from-q",
                         f"--extracter=FilmworkExtracter --batch-size={batch_size}",
-                        f"--source-queue={queue_name} --target-queue=fw",
+                        f"--source-queue={queue_name}-{XComArg(init_node, key='id')}",
+                        "--target-queue=fw-{XComArg(init_node, key='id')}",
                     ]
                 ),
                 dag=dag,
             )
-        else:
-            fill_fw_q_task = BashOperator(
-                task_id="empty-extract-fw-base-idx",
-                bash_command="echo 1",
-                dag=dag,
+            (
+                init_node
+                >> fill_base_task
+                >> fill_fw_q_task
+                >> [fill_fw_es, clean_q]
+                >> final_node
             )
-        fill_base_task >> fill_fw_q_task >> fill_fw_es
+        else:
+            init_node >> fill_base_task >> fill_fw_es >> clean_q >> final_node
